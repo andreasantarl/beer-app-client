8ceeecc27e97e1732730f7157052e0288ed8df51
andreasantarl <andrea.santarl@gmail.com>
2016-08-03 21:50:01 -0400

    Partway through installing rating plugin
    Needs to be fixed

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const app = {
-  host: 'http://localhost:3000',
-  // host: 'http://andrea-workout-tracker.herokuapp.com',
+  // host: 'http://localhost:3000',
+  host: 'http://beerbracket.herokuapp.com/',
 };
 
 module.exports = app;
