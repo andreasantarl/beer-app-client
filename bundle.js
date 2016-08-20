aceee8a6ba9ece4a521c1796e8cc2e9d3c5cca66
andreasantarl <andrea.santarl@gmail.com>
2016-08-20 16:09:01 -0400

    Bundle

--- bundle.js
+++ bundle.js
@@ -2084,7 +2084,7 @@ webpackJsonp([0],[
 	},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
 	    var stack1;
 
-	  return "<div class=\"sort-display-section\">\n  <form>\n    <fieldset class=\"form-group\">\n      <select id=\"sort-by\">\n        <option value=\"\">...</option>\n        <option value=\"beer_name\">Beer name</option>\n        <option value=\"company_name\">Brewer</option>\n        <option value=\"beer_style\">Style</option>\n        <option value=\"abv\">ABV</option>\n      </select>\n      <button type=\"submit\" class=\"sort-display btn btn-sm\">Sort me</button>\n    </fieldset>\n  </form>\n</div>\n\n\n<h2>{ Mine }</h2>\n<ul class='draggable'>\n\n"
+	  return "<div class=\"sort-display-section\">\n  <form>\n    <fieldset class=\"form-group\">\n      <select id=\"sort-by\">\n        <option value=\"\">...</option>\n        <option value=\"beer_name\">Beer name</option>\n        <option value=\"company_name\">Brewer</option>\n        <option value=\"beer_style\">Style</option>\n        <option value=\"abv\">ABV</option>\n      </select>\n      <button type=\"submit\" class=\"sort-display btn btn-md\">Sort me</button>\n    </fieldset>\n  </form>\n</div>\n\n\n<h2>{ Mine }</h2>\n<ul class='draggable'>\n\n"
 	    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.beers : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
 	    + "</ul>\n";
 	},"useData":true});
@@ -2154,7 +2154,7 @@ webpackJsonp([0],[
 	},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
 	    var stack1;
 
-	  return "<div class=\"sort-display-section\">\n  <form>\n    <fieldset class=\"form-group\">\n      <select id=\"sort-by\">\n        <option value=\"\">...</option>\n        <option value=\"beer_name\">Beer name</option>\n        <option value=\"company_name\">Brewer</option>\n        <option value=\"beer_style\">Style</option>\n        <option value=\"abv\">ABV</option>\n      </select>\n      <button type=\"submit\" class=\"sort-display-everyone btn btn-sm\">Sort me</button>\n    </fieldset>\n  </form>\n</div>\n\n<h2>{ Theirs }</h2>\n\n<ul class='draggable'>\n\n"
+	  return "<div class=\"sort-display-section\">\n  <form>\n    <fieldset class=\"form-group\">\n      <select id=\"sort-by\">\n        <option value=\"\">...</option>\n        <option value=\"beer_name\">Beer name</option>\n        <option value=\"company_name\">Brewer</option>\n        <option value=\"beer_style\">Style</option>\n        <option value=\"abv\">ABV</option>\n      </select>\n      <button type=\"submit\" class=\"sort-display-everyone btn btn-md\">Sort me</button>\n    </fieldset>\n  </form>\n</div>\n\n<h2>{ Theirs }</h2>\n\n<ul class='draggable'>\n\n"
 	    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.beers : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
 	    + "</ul>\n";
 	},"useData":true});
